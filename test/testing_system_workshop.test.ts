b69b49d0a36f85069f4268ed8bd0a63d448cfe4e
Jan Wiemers <jawie@amazon.de>
2023-04-17 14:54:31 +0200

    Initial commit

---
+++ test/testing_system_workshop.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TestingSystemWorkshop from '../lib/testing_system_workshop-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/testing_system_workshop-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TestingSystemWorkshop.TestingSystemWorkshopStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
